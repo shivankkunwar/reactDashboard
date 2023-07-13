1775011e79021ded37046797e40aba32506412dd
shivank <shivankkunwar100@gmail.com>
2023-07-13 16:35:07 +0530

    login form updated

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./Login.css";
+import google from "../icons/google.svg"
+import apple from "../icons/apple.svg"
 function Login() {
   return (
     <div className="main-container">
@@ -11,12 +13,20 @@ function Login() {
             <h1 className="signin">Sign In</h1>
             <p className="subtitle">Sign into your account</p>
             <div className="button-row">
-                <button className="google-btn">Login with Google</button>
-                <button className="apple-btn">Login with Apple</button>
+                <button className="google-btn"> 
+                
+                <img src={google} className='icons' alt="" />
+                 Login with Google
+                </button>
+                <button className="apple-btn">
+                <img src={apple} className='icons'alt="" />
+                Login with Apple</button>
             </div>
             <form className="login-form">
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <label htmlFor="">Email address</label>   
+            <input type="enter" placeholder="enter email address" />
+            <label htmlFor="">Password</label>   
+            <input type="password" placeholder="enter password" />
             <a href="#" className="forgot-password">Forgot password?</a>
             <button type="submit" className="signin-btn">Sign In</button>
             </form>
